e9a02f2766658e66142953a0dbec8ba5acf634ed
ldonghuan <ldonghuan6@gmail.com>
2026-05-13 22:06:07 +0800

    Revert base path to root for Cloudflare Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Configured for GitHub Pages deployment
-  base: "/blog/", 
+  // Configured for Cloudflare Pages (Root domain)
+  base: "/",
 })
